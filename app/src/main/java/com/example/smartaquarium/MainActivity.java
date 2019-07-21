a5ffa0157abfe88c75c4252cfbf1661f1df3ab31
Abi180798 <habiburrahman180798@gmail.com>
2019-07-21 12:07:04 +0800

    Smart-Aquarium-with-googleAssistant

--- app/src/main/java/com/example/smartaquarium/MainActivity.java
+++ app/src/main/java/com/example/smartaquarium/MainActivity.java
@@ -1,15 +1,27 @@
 package com.example.smartaquarium;
 
+import android.Manifest;
 import android.app.Notification;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -21,20 +33,95 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.InstanceIdResult;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Locale;
+
+import ai.api.AIListener;
+import ai.api.android.AIConfiguration;
+import ai.api.android.AIService;
+import ai.api.model.AIError;
+import ai.api.model.AIResponse;
+import ai.api.model.Result;
+
 import static com.example.smartaquarium.App.PEMBERITAHUAN;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AIListener{
+    AIService aiService;
     public static TextView status, action;
+    private static final int RECORD_REQUEST_CODE = 101;
+    private TextToSpeech textToSpeech;
     FirebaseDatabase database;
     DatabaseReference myRef, myReff, myRefff;
     private NotificationManagerCompat notificationManagerCompat;
     Button btn;
-
+    TextView hasil, hasil1;
+    String str;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        hasil = findViewById(R.id.hasil);
+        hasil1 = findViewById(R.id.hasil1);
+        int permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECORD_AUDIO);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            Log.i("permissionn", "Permission to record denied");
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.RECORD_AUDIO)) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage("Permission to access the microphone is required for this app to record audio.")
+                        .setTitle("Permission required");
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int id) {
+                        Log.i("clic", "Clicked");
+                        makeRequest();
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            } else {
+                makeRequest();
+            }
+        }
+        textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS){
+                    textToSpeech.setLanguage(Locale.ENGLISH);
+                    int result = textToSpeech.setLanguage(Locale.GERMAN);
+
+                    if (result == TextToSpeech.LANG_MISSING_DATA
+                            || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Log.e("TTS", "Language not supported");
+                    } else {
+                        btn.setEnabled(true);
+                    }
+                }else {
+                    Log.e("TTS", "Initialization failed");
+                }
+            }
+        });
+
+        final AIConfiguration config = new AIConfiguration("6195941710814115b441d85391a59668",
+                AIConfiguration.SupportedLanguages.English,
+                AIConfiguration.RecognitionEngine.System);
+        aiService = AIService.getService(this, config);
+        aiService.setListener(this);
 
         notificationManagerCompat = NotificationManagerCompat.from(this);
 
@@ -63,7 +150,8 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                myReff.setValue(1);
+
+                promptSpeechInput();
             }
         });
 
@@ -108,6 +196,242 @@ public class MainActivity extends AppCompatActivity {
             notificationManagerCompat.cancelAll();
         }else {
             notificationManagerCompat.notify(1, notification);
+        }
+    }
+    private void speak(String s) {
+        String text = s;
+//        Log.d("textx", text);
+//        float pitch = (float) mSeekBarPitch.getProgress() / 50;
+//        if (pitch < 0.1) pitch = 0.1f;
+//        float speed = (float) mSeekBarSpeed.getProgress() / 50;
+//        if (speed < 0.1) speed = 0.1f;
+//
+//        textToSpeech.setPitch(pitch);
+//        textToSpeech.setSpeechRate(speed);
+
+        textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+    }
+    @Override
+    protected void onDestroy() {
+        if (textToSpeech != null) {
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
+
+        super.onDestroy();
+    }
+
+    private void promptSpeechInput() {
+
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        // intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                "Say Something");
+        try {
+            startActivityForResult(intent, RECORD_REQUEST_CODE);
+
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    "Sorry! Your device doesn\\'t support speech input",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+    protected void makeRequest() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.RECORD_AUDIO},
+                RECORD_REQUEST_CODE);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case RECORD_REQUEST_CODE: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    String userQuery=result.get(0);
+                    hasil.setText(userQuery);
+                    Log.d("userquery", result.get(0));
+                    RetrieveFeedTask task=new RetrieveFeedTask();
+                    task.execute(userQuery);
+                    Log.d("twtwtw", hasil1.getText().toString());
+
+                }
+                break;
+            }
+
+        }
+    }
+
+    public String GetText(String query) throws UnsupportedEncodingException {
+
+        String text = "";
+        BufferedReader reader = null;
+
+        // Send data
+        try {
+
+            // Defined URL  where to send data
+            URL url = new URL("https://api.api.ai/v1/query?v=20150910");
+
+            // Send POST data request
+
+            URLConnection conn = url.openConnection();
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+
+            conn.setRequestProperty("Authorization", "Bearer 6195941710814115b441d85391a59668");
+            conn.setRequestProperty("Content-Type", "application/json");
+
+            //Create JSONObject here
+            JSONObject jsonParam = new JSONObject();
+            JSONArray queryArray = new JSONArray();
+            queryArray.put(query);
+            jsonParam.put("query", queryArray);
+//            jsonParam.put("name", "order a medium pizza");
+            jsonParam.put("lang", "en");
+            jsonParam.put("sessionId", "1234567890");
+
+
+            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+            Log.d("karma", "after conversion is " + jsonParam.toString());
+            wr.write(jsonParam.toString());
+            wr.flush();
+            Log.d("karma", "json is " + jsonParam);
+
+            // Get the server response
+
+            reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+
+
+            // Read Server Response
+            while ((line = reader.readLine()) != null) {
+                // Append server response in string
+                sb.append(line + "\n");
+            }
+
+
+            text = sb.toString();
+
+
+
+            JSONObject object1 = new JSONObject(text);
+            JSONObject object = object1.getJSONObject("result");
+            JSONObject fulfillment = null;
+            String speech = null;
+//            if (object.has("fulfillment")) {
+            fulfillment = object.getJSONObject("fulfillment");
+//                if (fulfillment.has("speech")) {
+            speech = fulfillment.optString("speech");
+//                }
+//            }
+            str = speech;
+
+            Log.d("karma ", "response is " + text);
+            return speech;
+
+        } catch (Exception ex) {
+            Log.d("karma", "exception at last " + ex);
+        } finally {
+            try {
+
+                reader.close();
+            } catch (Exception ex) {
+            }
+        }
+
+        return null;
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case RECORD_REQUEST_CODE: {
+
+                if (grantResults.length == 0
+                        || grantResults[0] !=
+                        PackageManager.PERMISSION_GRANTED) {
+
+                    Log.i("per1", "Permission has been denied by user");
+                } else {
+                    Log.i("per2", "Permission has been granted by user");
+                }
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void onResult(ai.api.model.AIResponse result) {
+        Log.d("resultnya", result.toString());
+        Result result1 = result.getResult();
+        hasil.setText("Query= " + result1.getResolvedQuery() + " Action= " + result1.getAction());
+        Log.d("hasill", result1.getAction());
+    }
+
+
+
+    @Override
+    public void onError(AIError error) {
+
+    }
+
+    @Override
+    public void onAudioLevel(float level) {
+
+    }
+
+    @Override
+    public void onListeningStarted() {
+
+    }
+
+    @Override
+    public void onListeningCanceled() {
+
+    }
+
+    @Override
+    public void onListeningFinished() {
+
+    }
+    class RetrieveFeedTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... voids) {
+            String s = null;
+            try {
+
+                s = GetText(voids[0]);
+                Log.d("hasils", s);
+                if (s.equals("controlled successfully")){
+                    myReff.setValue(1);
+                }
+                speak(s);
+
+
+
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+                Log.d("karma", "Exception occurred " + e);
+            }
+
+            return s;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            hasil1.setText(s);
+
         }
     }
 }
